d66637ea7f221600c52766cdcc176cf108f92018
Nathan Joel <nathanjoel1@hotmail.co.uk>
2024-01-10 12:01:18 +0000

    Book displayed Via DOM

--- script.js
+++ script.js
@@ -18,4 +18,41 @@ function addBookToLibrary() {
     console.log(myLibrary)
 }
 
-addBookToLibrary()
+function displayBooks() {
+
+    for (let i = 0; i < myLibrary.length; i++) {
+
+        const bookList = document.getElementsByClassName('book-list')
+
+        const card = document.createElement('div')
+        card.className = 'card'
+
+        const titleElement = document.createElement('p')
+        titleElement.className = 'title'
+        const text = document.createTextNode(myLibrary[i]['title'])
+        titleElement.appendChild(text)
+
+        const authorElement = document.createElement('p')
+        const author = document.createTextNode(myLibrary[i]['author'])
+        authorElement.appendChild(author)
+
+        const pagesElement = document.createElement('p')
+        const pages = document.createTextNode(myLibrary[i]['pages'])
+        pagesElement.appendChild(pages)
+
+        const readElement = document.createElement('p')
+        const read = document.createTextNode(myLibrary[i]['read'])
+        readElement.appendChild(read)
+
+        card.appendChild(titleElement)
+        card.appendChild(authorElement)
+        card.appendChild(pagesElement)
+        card.appendChild(readElement)
+
+        bookList[0].appendChild(card)
+    }
+}
+for (let i = 0; i < 20; i++) {
+    addBookToLibrary()
+}
+displayBooks()
