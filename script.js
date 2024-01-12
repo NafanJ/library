10dad2e128850f72bbbb986c7aa8f01b7c5405f5
Nathan Joel <nathanjoel1@hotmail.co.uk>
2024-01-12 15:42:32 +0000

    Simplified Card DOM Creation

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const button = document.getElementById("add-book")
+const bookList = document.querySelector('.book-list');
 const myLibrary = [];
 
 class Book {
@@ -11,80 +12,73 @@ class Book {
 }
 
 function addBookToLibrary(title, author, pages, read) {
-    const book = new Book(
-        title,
-        author,
-        pages,
-        read);
+    const book = new Book(title, author, pages, read);
     myLibrary.push(book)
 }
 
-addBookToLibrary(
-    'Crime And Punishment',
-    'Fyodor Dostoevsky',
-    720,
-    true)
-addBookToLibrary(
-    '1984',
-    'George Orwell',
-    449,
-    false)
-addBookToLibrary(
-    'To Kill A Mockingbird',
-    'Harper Lee',
-    398,
-    true)
+for (let i = 0; i < 20; i++) {
+    addBookToLibrary(
+        'Crime And Punishment',
+        'Fyodor Dostoevsky',
+        720,
+        true
+    );
+
+    addBookToLibrary(
+        '1984',
+        'George Orwell',
+        449,
+        false
+    );
+
+    addBookToLibrary(
+        'To Kill A Mockingbird',
+        'Harper Lee',
+        398,
+        true
+    );
+}
+
+
+function createTextElement(tag, className, text) {
+    const element = document.createElement(tag);
+    element.className = className;
+    element.textContent = text;
+    return element;
+}
+
+function createBookCard(book, index) {
+    const card = document.createElement('div');
+    card.className = 'card';
+
+    const titleElement = createTextElement('p', 'title', book.title);
+    const authorElement = createTextElement('p', 'author', book.author);
+    const pagesElement = createTextElement('p', 'pages', `${book.pages} pages`);
+    const readElement = createTextElement('p', 'read', book.read);
+
+    const removeButton = document.createElement('button');
+    removeButton.className = 'remove-button';
+    removeButton.textContent = 'Remove';
+    removeButton.addEventListener('click', () => {
+        myLibrary.splice(index, 1);
+        displayBooks();
+    });
+
+    card.appendChild(titleElement);
+    card.appendChild(authorElement);
+    card.appendChild(pagesElement);
+    card.appendChild(readElement);
+    card.appendChild(removeButton);
+
+    return card;
+}
 
 function displayBooks() {
-    const bookList = document.getElementsByClassName('book-list')
-    // Clear list before updating with new entries - prevents dup
-    bookList[0].innerHTML = ''
-
-    for (let i = 0; i < myLibrary.length; i++) {
-
-        const card = document.createElement('div')
-        card.className = 'card'
-
-        const titleElement = document.createElement('p')
-        titleElement.className = 'title'
-        const text = document.createTextNode(myLibrary[i]['title'])
-        titleElement.appendChild(text)
-
-        const authorElement = document.createElement('p')
-        const author = document.createTextNode(myLibrary[i]['author'])
-        authorElement.appendChild(author)
-
-        const pagesElement = document.createElement('p')
-        const pages = document.createTextNode(myLibrary[i]['pages'] + ' pages')
-        pagesElement.appendChild(pages)
-
-        const readElement = document.createElement('p')
-        const read = document.createTextNode(myLibrary[i]['read'])
-        readElement.appendChild(read)
-
-        const removeElement = document.createElement('button')
-        removeElement.className = 'remove-button'
-        removeElement.index = i
-
-        const remove = document.createTextNode('Remove');
-        removeElement.appendChild(remove)
-
-        removeElement.addEventListener("click", function () {
-            console.log(removeElement.index);
-            if (removeElement.index > -1) {
-                myLibrary.splice(removeElement.index, 1);
-            }
-            displayBooks()
-        });
-
-        card.appendChild(titleElement)
-        card.appendChild(authorElement)
-        card.appendChild(pagesElement)
-        card.appendChild(readElement)
-        card.appendChild(removeElement)
-
-        bookList[0].appendChild(card)
-    }
+    bookList.innerHTML = '';
+    myLibrary.forEach((book, index) => {
+        const card = createBookCard(book, index);
+        bookList.appendChild(card);
+    });
 }
 
 displayBooks();
